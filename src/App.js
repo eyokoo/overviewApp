f59263a15aa55cbe7e403902df449b47495dfddf
Emi Yokoo Mantilla <emimantilla@Christians-Air.lan1>
2021-06-03 23:46:42 -0500

    added onSubmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //state initialization
+    //where we define our own properties
+    this.state = {
+      isClicked: false,
+      inputValue: "",
+      listOfToDo: []
+    }
+    //binding 
+    this.onClickHandler = this.onClickHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onClickHandler() {
+    this.state.isClicked ?
+      this.setState({ isClicked: false }) :
+      this.setState({ isClicked: true })
+  }
+  onChangeHandler(event) {
+    this.setState({ inputValue: event.target.value }) //store this input value into the state
+  }
+
+  onSubmitHandler(event) {
+    event.preventDefault()
+    this.setState({ listOfToDo: [...this.state.listOfToDo, this.state.inputValue] })
+    this.setState({ inputValue: "" })
+  }
+
+  render() {
+
+    return (
+      <>
+        <form onSubmit={this.onSubmitHandler}>
+          <input type="text" value={this.state.inputValue} onChange={this.onChangeHandler} />
+          <button type="submit"> Submit </button>
+        </form>
+        <button onClick={this.onClickHandler}> Add To-Do </button>
+        {/* Display your todos found in your array */}
+      </>
+    );
+  }
 }
-
-export default App;
